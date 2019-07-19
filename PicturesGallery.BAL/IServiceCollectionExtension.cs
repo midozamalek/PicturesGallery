10d5d7aac842a0b640d9efc6e868845c6028194c
t-agouda@efg-hermes.com <t-agouda@efg-hermes.com>
2019-07-19 17:58:56 +0200

    setup DI for BAL

---
+++ PicturesGallery.BAL/IServiceCollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PicturesGallery.BAL
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection configureServices(this IServiceCollection services)
+        {
+             return services;
+        }
+    }
+}
